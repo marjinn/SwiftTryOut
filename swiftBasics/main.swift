851e3831ae137a464de79e92ea6f679a6fcc3372
mar Jinn <marjinn@mars-Mac.local>
2014-06-08 22:43:10 +0530

    Initial Commit

---
+++ swiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftBasics
+//
+//  Created by mar Jinn on 6/8/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
