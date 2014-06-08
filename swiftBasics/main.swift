e3e05e011d85c6bf17dcb028a3e4c6ed5fa07d8f
mar Jinn <marjinn@mars-Mac.local>
2014-06-08 23:52:34 +0530

    "first Commit"

--- swiftBasics/main.swift
+++ swiftBasics/main.swift
@@ -10,3 +10,98 @@ import Foundation
 
 println("Hello, World!")
 
+/*************/
+/* Optionals */
+/**************/
+
+//NON OPTIONAL TYPES CANNOT BE NIL
+
+var optionalNumber: Int?
+//default value in "nil"
+optionalNumber = 6
+
+/**
+retrieving an optional  value
+**/
+if(optionalNumber != nil)
+{
+    println("\(optionalNumber!)")
+}
+else
+{
+    println("\"(optionalNumber!)\"")
+}
+
+//################################
+//Function
+//###############################
+func findIndexOfString(string:String, array:String[]) -> Int?
+{
+    for (index:Int,value:String) in enumerate(array)
+    {
+        if(value == string)
+        {
+            return index;
+        }
+    }
+    
+    return nil
+}
+
+var neighbours:String[];
+neighbours = ["Alex","Anna","Madison","Dave"];
+
+//function returns an optional integer
+//so assign it to an optional variable
+let index:Int? = findIndexOfString("Madison", neighbours);
+
+//check for nil
+if (index != nil)
+{
+    println("Hello , \(neighbours[index!])");
+}
+else
+{
+    println("Must've moved away");
+}
+
+//OR use a temp non optional var
+if let indexValue:Int = index
+{
+    println("Hello , \(neighbours[indexValue])");
+    
+}
+else
+{
+    println("Must've moved away");
+}
+
+
+
+/********************/
+/* Optional binding */
+/********************/
+// uses the "if let" operator - tests and unwraps
+// the variable used after "if let" is not of optional type
+// also you cannot have "if let" in parantheses
+// You dont have to force unwrap using unwrap (!) opeartor
+
+if let __index:Int = findIndexOfString("Madison", neighbours)
+{
+    println("Hello , \(neighbours[__index])");
+    
+}
+else
+{
+    println("Must've moved away");
+}
+
+
+
+
+
+
+
+
+
+
